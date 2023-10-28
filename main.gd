581d5e04521ea3e5cc5c6faa49f81192245c3f07
Dh-ux <lxiong19@ucsc.edu>
2023-10-28 11:55:28 -0400

    1

--- main.gd
+++ main.gd
@@ -1,12 +1,6 @@
 extends Node2D
 
 
-
-@onready var block1 = $Node2/block1
-@onready var block2 = $Node2/block2
-@onready var block3 = $Node2/block3
-@onready var block4 = $Node2/block4
-@onready var buttons = $Node.get_children()
 #depricated
 
 @onready var timer = $Timer
@@ -17,7 +11,7 @@ var target_color = "Red"
 var target_button = null
 
 func _ready():
-	timer.start(1) 
+	timer.start(4.6) 
 
 func _timeout():
 	if color_types.size() !=0:
@@ -40,7 +34,7 @@ func _timeout():
 			$hint/Sprite2D.texture = load("res://resources/art/yellowcolor.jpg")
 		
 		$hint.visible = true
-		timer.start(2)
+		timer.start(4)
 	else:
 		$hint.visible = false
 		game_over()
@@ -64,113 +58,22 @@ func color_picked(target):
 func game_over():
 	pass
 
-func _all_button_disabled():
-	for button in buttons:
-		if not button.disabled:
-			return false
-	return true		
-	
-	
-func _on_1_pressed():
-#	var color_matched = false
-#	for button in red_blocks:
-#		if button == target_button:
-#			color_matched = true
-#			break
-	if $Node/picker1 == target_button:
-		print("Correct!")
-		print("color 1 is picked")
-		target_button.disabled = true
-		$Node/picker1/block1.texture = load("res://resources/art/yellownocolor.jpg")
-		$Node2/block2.texture = load("res://resources/art/redcolor.jpg")
-		$Node/picker1.disabled = true
-	else:
-		pass	
-	
-
-func _on_2_pressed():
-	if $Node/picker2 == target_button:
-		print("Correct!")
-		print("color 2 is picked")
-		$Node/picker2/block2.texture = load("res://resources/art/rednocolor.jpg")
-		$Node2/block3.texture = load("res://resources/art/bluecolor.jpg")
-		$Node/picker2.disabled = true
-	else:
-		pass	
-
-
-func _on_3_pressed():
-	if $Node/picker3 == target_button:
-		print("Correct!")
-		print("color 3 is picked")
-		$Node/picker3/block3.texture = load("res://resources/art/bluenocolor.jpg")
-		$Node2/block4.texture = load("res://resources/art/greencolor.jpg")
-		$Node/picker3.disabled = true
-	else:
-		pass	
-	
-
-func _on_4_pressed():
-	if $Node/picker4 == target_button:
-		print("Correct!")
-		print("color 4 is picked")
-		$Node/picker4/block4.texture = load("res://resources/art/greennocolor.jpg")
-		$Node2/block1.texture = load("res://resources/art/yellowcolor.jpg")
-		$Node/picker4.disabled = true
-	else:
-		pass	
-	
-
-func _on_picker_1_ready():
-	pass
-
-
-func _on_picker_2_ready():
-	pass # Replace with function body.
-
-
-func _on_picker_3_ready():
-	pass # Replace with function body.
-
-
-func _on_picker_4_ready():
-	pass # Replace with function body.
-
-
-func _on_picker_1_pressed():
-	_on_1_pressed()
-
-func _on_picker_2_pressed():
-	_on_2_pressed()
-
-
-func _on_picker_3_pressed():
-	_on_3_pressed()
-
-
-func _on_picker_4_pressed():
-	_on_4_pressed()
 
 
 func _on_timer_timeout():
-	if _all_button_disabled():
-		get_tree().quit()
-	else:	
-		_timeout()
+	_timeout()
 
 
-func _on_picker_5_pressed():
-	pass # Replace with function body.
 
 
-func _on_area_2d_input_event(viewport, event, shape_idx):
-	if (event is InputEventMouseButton && event.pressed):
-		if $Node/picker1 == target_button:
-			print("Correct!")
-			print("color 1 is picked")
-			target_button.disabled = true
-			$Node/picker1/block1.texture = load("res://resources/art/yellownocolor.jpg")
-			$Node2/block2.texture = load("res://resources/art/redcolor.jpg")
-			$Node/picker1.disabled = true
-		else:
-			pass	
+#func _on_area_2d_input_event(viewport, event, shape_idx):
+#	if (event is InputEventMouseButton && event.pressed):
+#		if $Node/picker1 == target_button:
+#			print("Correct!")
+#			print("color 1 is picked")
+#			target_button.disabled = true
+#			$Node/picker1/block1.texture = load("res://resources/art/yellownocolor.jpg")
+#			$Node2/block2.texture = load("res://resources/art/redcolor.jpg")
+#			$Node/picker1.disabled = true
+#		else:
+#			pass	
