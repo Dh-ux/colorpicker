70bae45a4a488936b01d9781a62c6432955ee2a9
EiZi233 <57603782+EiZi233@users.noreply.github.com>
2023-10-28 13:19:43 -0400

    update track

---
+++ Track.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var enabled = false
+
+func _ready():
+	pass # Replace with function body.
+
+func start():
+	enabled = true
+	$key.position = Vector2(-200,0)
+
+func _process(delta):
+	if enabled:
+		$key.position += Vector2(1,0)*100*delta
+		
+
+func reset():
+	enabled = false
+	$key.position = Vector2(-200,0)
--- main.gd
+++ main.gd
@@ -10,10 +10,24 @@ var target_color = "Red"
 
 var target_button = null
 
+var beat = 0
+var internal_beat = 0
+var beat_speed = 0.5
+var started = false
+
+var hint_ref
+
 func _ready():
 	timer.start(4.6) 
-
-func _timeout():
+	hint_ref = $Track/key/hint
+	
+func on_beats():
+	internal_beat += 1
+	if beat%8 == 0:
+		internal_beat = 0
+		on_bar()
+		
+func on_bar():
 	if color_types.size() !=0:
 		var color_index = randi()%color_types.size()
 		target_color = color_types[color_index]
@@ -21,23 +35,35 @@ func _timeout():
 	#	if colortype['Red'] == 0:
 	#		#color_types.erase(target_color)
 		color_types.erase(target_color)
-		
+		$Track.start()
 		print(target_color)
 		
 		if target_color == 'Red':
-			$hint/Sprite2D.texture = load("res://resources/art/redcolor.jpg")
+			hint_ref.texture = load("res://resources/art/redcolor.jpg")
 		elif target_color == 'Blue':
-			$hint/Sprite2D.texture = load("res://resources/art/bluecolor.jpg")
+			hint_ref.texture = load("res://resources/art/bluecolor.jpg")
 		elif target_color == 'Green':
-			$hint/Sprite2D.texture = load("res://resources/art/greencolor.jpg")
+			hint_ref.texture = load("res://resources/art/greencolor.jpg")
 		elif target_color == 'Yellow':
-			$hint/Sprite2D.texture = load("res://resources/art/yellowcolor.jpg")
+			hint_ref.texture = load("res://resources/art/yellowcolor.jpg")
 		
-		$hint.visible = true
-		timer.start(4)
+		hint_ref.visible = true
+		#timer.start(4)
 	else:
-		$hint.visible = false
+		hint_ref.visible = false
+		$Track.reset()
 		game_over()
+
+func create_key():
+	pass
+
+func _timeout():
+	if not started:
+		started = true
+		on_beats()
+		$BeatCounter.start(beat_speed)
+		$Track.start()
+		
 
 
 
@@ -77,3 +103,8 @@ func _on_timer_timeout():
 #			$Node/picker1.disabled = true
 #		else:
 #			pass	
+
+
+func _on_beat_counter_timeout():
+	beat += 1
+	on_beats()
