1c7b99c17d06406cf647c4be9399db5a649157f5
EiZi233 <57603782+EiZi233@users.noreply.github.com>
2023-10-29 01:28:16 -0400

    upsh

---
+++ startButton.gd
@@ -0,0 +1,19 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_mouse_entered():
+	set_modulate(Color(1.2,1.2,1.2,1))
+
+
+func _on_mouse_exited():
+	set_modulate(Color(1,1,1,1))
